d9d8da7365f23b6d54552097c253ab9b8bc1a690
Martin Tovmassian <martin.tovmassian@hardis.fr>
2017-11-24 15:29:38 +0100

    recast bc funcs to make bc act like a real bc

--- block.py
+++ block.py
@@ -2,31 +2,53 @@ import datetime
 import hashlib
 
 SHA = hashlib.sha256()
-gen_hash = lambda timestamp: (lambda x=(lambda: SHA.update(str(timestamp).encode()))(): SHA.hexdigest())()
+LIGTH_MAGENTA  = '\033[95m'
+DEFAULT = '\033[m'
 
 class Block:
 
     def __init__(self, data):
+        self.__data = data
         self.timestamp = datetime.datetime.now()
-        self.hash = gen_hash(self.timestamp)
-        self.previous_block = None
-        self.next_block = None
-        self.data = data
+        self.hash = self.gen_hash()
+        self.block_index = None
+        self.previous_block_hash = None
+        self.next_block_hash = None
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, new_data):
+        self.__data = new_data
+        self.timestamp = datetime.datetime.now()
+        self.hash = self.gen_hash()
+
+    def gen_hash(self):
+        return (lambda x=SHA.update(str({self.timestamp, self.__data}).encode()): SHA.hexdigest())()
 
     def __str__(self):
         width = 100
         sep_l, sep_h = '/', '//'
         block_edge = "{0}".format(sep_l * width)
+        block_index = "{0}    block_index: {1}".format(sep_h, self.block_index).ljust(width-2, ' ') + sep_h
         hash = "{0}    hash: {1}".format(sep_h, self.hash).ljust(width-2, ' ') + sep_h
         timestamp = "{0}    timestamp: {1}".format(sep_h, self.timestamp).ljust(width-2, ' ') + sep_h
-        previous_block_hash = "{0}    previous_block: {1}".format(
-            sep_h, None if self.previous_block == None else self.previous_block.hash).ljust(width-2, ' '
+        previous_block_hash = "{0}    previous_block_hash: {1}".format(
+            sep_h, None if self.previous_block_hash == None else self.previous_block_hash).ljust(width-2, ' '
         ) + sep_h
-        next_block_hash = "{0}    next_block: {1}".format(
-            sep_h, None if self.next_block == None else self.next_block.hash).ljust(width-2, ' '
+        next_block_hash = "{0}    next_block_hash: {1}".format(
+            sep_h, None if self.next_block_hash == None else self.next_block_hash).ljust(width-2, ' '
         ) + sep_h
         data = "{0}    data: {1}".format(sep_h, self.data).ljust(width-2, ' ') + sep_h
 
-        return "{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}".format(
-            block_edge, hash, timestamp, previous_block_hash, next_block_hash, data, block_edge
-        )
+        return LIGTH_MAGENTA + "{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}".format(
+            block_edge, block_index, hash, timestamp, previous_block_hash, next_block_hash, data, block_edge
+        ) + DEFAULT
+
+if __name__ == '__main__':
+    b = Block('alpha block')
+    print(b)
+    b.data = 'michel'
+    print(b)
--- blockchain.py
+++ blockchain.py
@@ -7,41 +7,59 @@ class Blockchain:
 
     def __init__(self):
         self.current_block = None
+        self.chain = []
         self.add_genesis_block()
 
     def add(self, data):
         new_block = Block(data=data)
-        self.current_block.next_block = new_block
-        new_block.previous_block = self.current_block
+        new_block.block_index = self.current_block.block_index + 1
+        self.current_block.next_block_hash = new_block.hash
+        new_block.previous_block_hash = self.current_block.hash
+        self.chain.append(new_block)
         self.current_block = new_block
 
     def add_genesis_block(self):
         self.current_block = Block('I am the genesis block.')
+        self.current_block.block_index = 0
+        self.chain.append(self.current_block)
 
     def get_current_block(self):
         return LIGTH_MAGENTA + "\n{0}\n".format(self.current_block) + DEFAULT
 
-    def go_through(self):
+    def search(self, block_index):
+        block = None
+        current_block = self.current_block
+        try:
+            block = next(block for block in self.chain if block.block_index == block_index)
+        except StopIteration:
+            print("/!\ No block found.")
+        return block
+
+    def go_down(self):
         current_block = self.current_block
         while current_block is not None:
-            input('[ENTER] to see previous block ')
+            input('[PREVIOUS] ')
             print(LIGTH_MAGENTA + "{0}".format(current_block) + DEFAULT)
             current_block = current_block.previous_block
 
     def __str__(self):
         current_block = self.current_block
-        string = ""
-        while current_block is not None:
-            string = "\n{0}".format(current_block) if string == "" else "{0}\n{1}\n{1}\n{2}".format(
-                string, '|'.center(100, ' '), current_block
-            )
-            current_block = current_block.previous_block
+        string = "\n{0}".format(current_block)
+        while current_block.previous_block_hash is not None:
+            try:
+                current_block = next(block for block in self.chain if block.hash == current_block.previous_block_hash)
+                string = "{0}\n{1}\n{1}\n{2}".format(string, LIGTH_MAGENTA + '|'.center(100, ' '), current_block)
+            except StopIteration:
+                string = "\n{0}\nCant't retrieve blocks before block {1}.\n".format("/!\ Blockchain is corrupted.", current_block.block_index)
+                break
         return LIGTH_MAGENTA + string + '\n' + DEFAULT
+
 
 if __name__ == '__main__':
     bc = Blockchain()
     bc.add('alpha block')
     bc.add('bravo block')
     bc.add('charly block')
+    bc.add('delta block')
 
-    print(bc.get_current_block())
+    print(bc)
